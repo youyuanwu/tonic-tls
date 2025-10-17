5cc068e254cc4271205a334266965d1b7a449c2d
Youyuan Wu <youyuanwu@outlook.com>
2025-10-16 21:27:35 -0700

    propagate tcp settings

--- tonic-tls/src/endpoint.rs
+++ tonic-tls/src/endpoint.rs
@@ -6,8 +6,9 @@ use hyper::Uri;
 /// in tls connector layer.
 pub(crate) struct TcpOpt {
     pub(crate) uri: Uri,
-    // interval is not supported in tonic yet.
     pub(crate) keep_alive_duration: Option<Duration>,
+    pub(crate) tcp_keepalive_interval: Option<Duration>,
+    pub(crate) tcp_keepalive_retries: Option<u32>,
     pub(crate) no_delay: bool,
 }
 
@@ -16,6 +17,8 @@ impl TcpOpt {
     pub(crate) fn from_ep(ep: &tonic::transport::Endpoint) -> Self {
         Self {
             keep_alive_duration: ep.get_tcp_keepalive(),
+            tcp_keepalive_interval: ep.get_tcp_keepalive_interval(),
+            tcp_keepalive_retries: ep.get_tcp_keepalive_retries(),
             no_delay: ep.get_tcp_nodelay(),
             uri: ep.uri().clone(),
         }
@@ -26,11 +29,36 @@ impl TcpOpt {
         if self.no_delay {
             tcp.set_nodelay(true)?;
         }
-        if let Some(keep_alive_duration) = self.keep_alive_duration {
-            let ka = socket2::TcpKeepalive::new().with_time(keep_alive_duration);
+        if let Some(keepalive) = Self::make_keepalive(
+            self.keep_alive_duration,
+            self.tcp_keepalive_interval,
+            self.tcp_keepalive_retries,
+        ) {
             let sf = socket2::SockRef::from(&tcp);
-            sf.set_tcp_keepalive(&ka)?
+            sf.set_tcp_keepalive(&keepalive)?
         }
         Ok(())
+    }
+
+    fn make_keepalive(
+        keepalive_time: Option<Duration>,
+        keepalive_interval: Option<Duration>,
+        keepalive_retries: Option<u32>,
+    ) -> Option<socket2::TcpKeepalive> {
+        let mut dirty = false;
+        let mut keepalive = socket2::TcpKeepalive::new();
+        if let Some(t) = keepalive_time {
+            keepalive = keepalive.with_time(t);
+            dirty = true;
+        }
+        if let Some(t) = keepalive_interval {
+            keepalive = keepalive.with_interval(t);
+            dirty = true;
+        }
+        if let Some(r) = keepalive_retries {
+            keepalive = keepalive.with_retries(r);
+            dirty = true;
+        }
+        dirty.then_some(keepalive)
     }
 }
